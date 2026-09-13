7bf3dd4ce0848b4d84b4dbe713fdfb7395a725b6
NourEldin <noureldindotdev@gmail.com>
2026-09-13 08:30:36 +0300

    Add basic checkmark logic

--- src/script.js
+++ src/script.js
@@ -1,28 +1,47 @@
-// const checkMark = document.querySelector('.topic-title__check');
-
-// // const gray = document.querySelectorAll('.gray');
-
-// console.log(checkMark)
-
-// function makeGreen() {
-//     if (checkMark.classList.contains('gray')) {
-//             checkMark?.classList.remove('gray')
-//         } else {
-//             checkMark?.classList.add('gray')
-//         }    
-// }
-
-// checkMark.addEventListener('click', makeGreen);
-
-
-document.querySelector('.notes')?.addEventListener('click', function(e) {
+document.querySelector('.notes').addEventListener('click', function(event) {
     
-    const target = e.target
+    const target = event.target
 
-    if (target.matches('.topic-title__check')) {
-        if (target.classList.contains('gray')) {
-            target.classList.remove('gray')
+    const nextSibling = target.nextElementSibling;
+
+    const parent = target.parentElement;
+
+    const grandParent = parent.parentElement;
+
+    const previuosParentSibling = grandParent.previousElementSibling;
+
+    const previuosSibling = parent.previousElementSibling
+
+
+    if (target.matches('.topic-title__unckecked')) {
+        if (target.classList.contains('hidden')) {
+            target.classList.remove('hidden')
+            nextSibling.classList.add('hidden')
         } else {
-            target.classList.add('gray')}  
+            target.classList.add('hidden')
+            nextSibling.classList.remove('hidden')
+        }
     }
+
+    if (target.matches('svg')) {
+        if (parent.classList.contains('hidden')) {
+            parent.classList.remove('hidden')
+            previuosSibling.classList.add('hidden')
+        } else {
+            parent.classList.add('hidden')
+            previuosSibling.classList.remove('hidden')
+        }
+    }
+    
+    if (target.matches('path')) {
+        console.log(previuosParentSibling)
+        if (previuosParentSibling.classList.contains('hidden')) {
+            previuosParentSibling.classList.remove('hidden')
+            grandParent.classList.add('hidden')
+        } else {
+            previuosParentSibling.classList.add('hidden')
+            grandParent.classList.remove('hidden')
+        }
+    }
+
 })
