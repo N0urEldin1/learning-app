dba10456547d7d9d22773162867b1bce9c492a1b
NourEldin <noureldindotdev@gmail.com>
2026-09-15 14:59:45 +0300

    Fix checkmark logic

--- src/script.js
+++ src/script.js
@@ -16,32 +16,91 @@ document.querySelector('.notes').addEventListener('click', function(event) {
     if (target.matches('.topic-title__unckecked')) {
         if (target.classList.contains('hidden')) {
             target.classList.remove('hidden')
-            nextSibling.classList.add('hidden')
+            if (nextSibling.classList.contains('topic-title__ckecked')) {
+                nextSibling.classList.add('hidden')
+            }
         } else {
             target.classList.add('hidden')
-            nextSibling.classList.remove('hidden')
+            if (nextSibling.classList.contains('topic-title__ckecked')) {
+                nextSibling.classList.remove('hidden')
+            }
         }
     }
 
     if (target.matches('svg')) {
-        if (parent.classList.contains('hidden')) {
-            parent.classList.remove('hidden')
-            previuosSibling.classList.add('hidden')
-        } else {
-            parent.classList.add('hidden')
-            previuosSibling.classList.remove('hidden')
+        if (parent.classList.contains('topic-title__unckecked')) {
+            if (parent.classList.contains('hidden')) {
+                parent.classList.remove('hidden')
+                previuosSibling.classList.add('hidden')
+            } else {
+                parent.classList.add('hidden')
+                previuosSibling.classList.remove('hidden')
+            }
         }
     }
     
     if (target.matches('path')) {
-        console.log(previuosParentSibling)
-        if (previuosParentSibling.classList.contains('hidden')) {
-            previuosParentSibling.classList.remove('hidden')
-            grandParent.classList.add('hidden')
-        } else {
-            previuosParentSibling.classList.add('hidden')
-            grandParent.classList.remove('hidden')
+        if (previuosParentSibling.classList.contains('topic-title__unckecked')) {
+            if (previuosParentSibling.classList.contains('hidden')) {
+                previuosParentSibling.classList.remove('hidden')
+                grandParent.classList.add('hidden')
+            } else {
+                previuosParentSibling.classList.add('hidden')
+                grandParent.classList.remove('hidden')
+            } 
         }
-    }
-
+    } 
 })
+
+
+// Progress bar
+
+// Total
+const notes = document.querySelectorAll('.note')
+const noteCount = notes.length;
+
+// Checked
+const isChecked = document.querySelectorAll('h')
+
+
+// const checkMarks = document.getElementsByClassName('topic-title')
+
+// console.log(checkMarks)
+
+// let checkedCount = 0;
+
+// for (let unCheckedNote of unChecked) {
+
+//     let checkedCount = 0;
+
+//     if (!unCheckedNote.classList.contains('hidden')) {
+//         checkedCount += 1
+//     }
+
+// }
+
+// console.log(checkedCount)
+// console.log(unChecked.length)
+
+
+// Green line logic
+const progressLine = document.querySelector('.line__green')
+
+const value = noteCount * 10
+
+progressLine.setAttribute('style', 'width: ' + value + '%')
+
+// progress text logic
+
+
+// percentage text logic
+
+const percentageTextElement = document.querySelector('.progress-bar__percentage')
+
+let percentageText = percentageTextElement.innerText
+
+percentageText = `${value}%`
+
+percentageTextElement.innerText = percentageText
+
+// console.log(percentageText)
